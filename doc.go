0f58f7382c541aaf9050a1dce36552a5439f8b9a
Igor Zibarev <zibarev.i@gmail.com>
2017-11-17 17:56:30 +0300

    Change package name

---
+++ doc.go
@@ -0,0 +1,8 @@
+// Package oas provides utilities to work with OpenAPI 2.0 specification
+// (aka Swagger).
+//
+// The purpose of this package is to provide utilities for building APIs
+// from the OpenAPI specification in Go idiomatic way on top of `net/http`.
+// The package can handle request validation, request parameters decoding
+// and other routines.
+package oas
