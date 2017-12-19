91661250b48a86ef6576fde075ff1d8d623555a1
Valerij <Valep993@gmail.com>
2017-12-19 12:00:22 +0300

    If http-header not json do not unmarshal it

--- response_body_validator.go
+++ response_body_validator.go
@@ -21,6 +21,14 @@ type responseBodyValidator struct {
 
 func (m responseBodyValidator) Apply(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		if w.Header().Get("Content-Type") != "application/json" {
+			// Do not validate multipart/form.
+			// There will be built-in validation in oas package,
+			// but currently it's cumbersome to implement.
+			next.ServeHTTP(w, req)
+			return
+		}
+
 		op := GetOperation(req)
 		if op == nil {
 			next.ServeHTTP(w, req)
